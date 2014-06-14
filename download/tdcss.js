ebd3b7b01214dd6809d13fce50c94a568284d250
Rob Levin <pair+rob@mavenlink.com>
2014-06-14 08:26:58 -0700

    Allows a way to opt-out of storing url hash since it causes page jumps.

--- download/tdcss.js
+++ download/tdcss.js
@@ -20,6 +20,7 @@
                 fragment_info_splitter: ";",
                 replacementContent: "...",
                 hideTextContent: false,
+                setCollapsedStateInUrl: true,
                 hideTheseAttributesContent: [
                     'src',
                     'href'
@@ -340,11 +341,15 @@
                         if (that.collapsed) {
                             $(that.header_element).addClass("is-collapsed");
                             $(that.fragments_in_section).hide();
-                            that.setCollapsedStateInUrl();
+                            if (settings.setCollapsedStateInUrl) {
+                                that.setCollapsedStateInUrl();
+                            }
                         } else {
                             $(that.header_element).removeClass("is-collapsed");
                             $(that.fragments_in_section).show();
-                            that.removeCollapsedStateFromUrl();
+                            if (settings.setCollapsedStateInUrl) {
+                                that.removeCollapsedStateFromUrl();
+                            }
                         }
                     };
 
